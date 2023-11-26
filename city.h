deb70b4e1af8b40f6910db02b5f903c1b63e522a
Andre Duran <andreduran1@yahoo.com>
2023-11-25 22:57:35 -0800

    .txt files added and class

---
+++ city.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class City{
+    public:
+    
+    private: 
+    int id;
+    string code;
+    string name;
+    int population;
+    int elevation;
+}
